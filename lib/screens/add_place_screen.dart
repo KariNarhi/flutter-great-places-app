c46d521cca6b69005764f0f93a3465fd719f7937
Kari Närhi <Kari.Narhi@tecinspire.com>
2021-10-30 17:25:49 +0300

    Managing Data & Images via Provider Package

--- lib/screens/add_place_screen.dart
+++ lib/screens/add_place_screen.dart
@@ -1,6 +1,9 @@
 import "package:flutter/material.dart";
+import 'dart:io';
+import 'package:provider/provider.dart';
 
 import '../widgets/image_input.dart';
+import '../providers/great_places.dart';
 
 class AddPlaceScreen extends StatefulWidget {
   static const routeName = "/add-place";
@@ -11,6 +14,23 @@ class AddPlaceScreen extends StatefulWidget {
 
 class _AddPlaceScreenState extends State<AddPlaceScreen> {
   final _titleController = TextEditingController();
+  File _pickedImage;
+
+  void _selectImage(File pickedImage) {
+    _pickedImage = pickedImage;
+  }
+
+  void _savePlace() {
+    if (_titleController.text.isEmpty || _pickedImage == null) {
+      return;
+    }
+
+    Provider.of<GreatPlaces>(context, listen: false).addPlace(
+      _titleController.text,
+      _pickedImage,
+    );
+    Navigator.of(context).pop();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -32,7 +52,7 @@ class _AddPlaceScreenState extends State<AddPlaceScreen> {
                       controller: _titleController,
                     ),
                     SizedBox(height: 10),
-                    ImageInput(),
+                    ImageInput(_selectImage),
                   ],
                 ),
               ),
@@ -41,7 +61,7 @@ class _AddPlaceScreenState extends State<AddPlaceScreen> {
           ElevatedButton.icon(
             icon: Icon(Icons.add),
             label: Text("Add Place"),
-            onPressed: () {},
+            onPressed: _savePlace,
             style: ButtonStyle(
               elevation: MaterialStateProperty.all(0),
               tapTargetSize: MaterialTapTargetSize.shrinkWrap,
--- lib/widgets/image_input.dart
+++ lib/widgets/image_input.dart
@@ -5,6 +5,10 @@ import 'package:path/path.dart' as path;
 import 'package:path_provider/path_provider.dart' as syspaths;
 
 class ImageInput extends StatefulWidget {
+  final Function onSelectImage;
+
+  ImageInput(this.onSelectImage);
+
   @override
   _ImageInputState createState() => _ImageInputState();
 }
@@ -28,6 +32,7 @@ class _ImageInputState extends State<ImageInput> {
       final appDir = await syspaths.getApplicationDocumentsDirectory();
       final fileName = path.basename(imageFile.path);
       final savedImage = _storedImage.copySync('${appDir.path}/$fileName');
+      widget.onSelectImage(savedImage);
     } else {
       print("No image selected");
     }
