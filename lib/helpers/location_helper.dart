b9aa7ea0546fb2f29f4c055670b6275fc6fa6058
Kari Närhi <Kari.Narhi@tecinspire.com>
2021-10-31 16:51:52 +0200

    Display a static Map Snapshot

---
+++ lib/helpers/location_helper.dart
@@ -0,0 +1,10 @@
+const GOOGLE_API_KEY = "AIzaSyDJZx0qCo2RyFm7paPtzudLvQiVjt31kms";
+
+class LocationHelper {
+  static String generateLocationPreviewImage({
+    double latitude,
+    double longitude,
+  }) {
+    return "https://maps.googleapis.com/maps/api/staticmap?center=$latitude,$longitude&zoom=16&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C$latitude,$longitude&key=$GOOGLE_API_KEY";
+  }
+}
